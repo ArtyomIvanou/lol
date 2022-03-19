134cd10a0c3d32ae7e3ed697d92561f911be990f
ArtsiomIvanou <ArtyomIvanou@yandex.by>
2022-03-19 21:30:28 +0300

    qqq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState } from 'react';
+
+
+let clientTypes = [
+  {
+    name: 'Новый клиент',
+    color: 'red',
+  },
+  {
+    name: 'На удаление',
+    color: 'yellow',
+  },
+  {
+    name: 'Пробное занятие',
+    color: 'green',
+  },
+  {
+    name: 'Не отвечает',
+    color: 'blue',
+  },
+]
+let client = { selectedType: 'Пробное занятие' }
+
+
+const App = () => {
+  const [value, selectedValue] = useState(client.selectedType)
+  const [isVisible, visibility] = useState(false)
+  const changeValue = (value) => {
+    selectedValue(value)
+    visibility(false)
+  }
+  const selectVisibility = (value) => {
+    visibility(!value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List selectedValue={value} clientTypes={clientTypes} changeValue={changeValue} isVisible={isVisible} selectVisibility={selectVisibility}></List>
     </div>
   );
 }
+
+
+const List = (props) => {
+  console.log(props.selectedValue)
+
+  let isVisible
+  let isVisibleCaret
+  if (props.isVisible) {
+    isVisible = 'visible_block'
+    isVisibleCaret = 'visible_caret'
+  } else {
+    isVisible = 'unvisible_block'
+    isVisibleCaret = 'unvisible_caret'
+  }
+  let seletedStyle
+  let typesOfClient = props.clientTypes.map((a, i) => {
+    const divStyle = {
+      backgroundColor: a.color,
+    };
+    if (a.name === props.selectedValue) {
+      seletedStyle = {
+        backgroundColor: a.color,
+      };
+      return <div value={a.name}
+        style={divStyle}
+        onClick={() => props.changeValue(a.name)}
+        className={`${'not_select'}${' '}${'for_select'}`}
+        key={a.name}><span className='selected_type_content'>{a.name}</span><span className='selected_type'></span></div>
+    } else {
+      return <div value={a.name}
+        style={divStyle}
+        onClick={() => props.changeValue(a.name)}
+        className={`${'not_select'}`}
+        key={a.name}><span>{a.name}</span></div>
+    }
+  })
+  return (
+    <div className='main'>
+      <div style={seletedStyle}
+        className={`${'not_select'}`}
+        onClick={() => props.selectVisibility(props.isVisible)}><span className={isVisibleCaret}>{props.selectedValue}</span><span></span></div>
+      <div className={isVisible}>{typesOfClient}</div>
+
+    </div>
+  );
+}
+
+
 
 export default App;
